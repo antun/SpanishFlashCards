555ed25e339962158bf1e9934dc009ada71643f6
Antun Karlovac <antun@antunkarlovac.com>
2012-10-20 20:15:59 -0700

    Words are now centered.
    Closes #2

--- app/view/Flashcard.js
+++ app/view/Flashcard.js
@@ -12,23 +12,36 @@ Ext.define("SFCT.view.Flashcard", {
         itemId: 'mainScreen',
         title: 'Flashcards',
 
-        styleHtmlContent: true,
+        layout: 'fit',
 
         items: [
-        {
-            docked: 'right',
-            xtype: 'button',
-            text: 'Next',
-            action: 'next'
-        }
-        ],
-
-        html: [
-        ].join("")
+            {
+                xtype: 'container',
+                itemId: 'wordContainer',
+                layout: {
+                    type: 'vbox',
+                    pack: 'center'
+                },
+                items: [
+                    {
+                        xtype: 'container',
+                        itemId: 'wordContainer'
+                    }
+                ],
+                style: 'text-align: center'
+            },
+            {
+                xtype: 'button',
+                docked: 'right',
+                text: 'Next',
+                action: 'next'
+            }
+        ]
     },
 
     updateWord: function(newWord) {
-        var s = '<div style="font-size: 72px; text-align: center">' + newWord+ '</div>';
-        this.setHtml(s);
+        var s = '<div style="font-size: 72px">' + newWord+ '</div>';
+        var c = Ext.ComponentQuery.query('#wordContainer')[0];
+        c.setHtml(s);
     }
 });
